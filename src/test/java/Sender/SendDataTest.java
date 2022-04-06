4226881d8f0e46889a133a33023d0b6c5deebc7a
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-04-06 20:36:20 +0530

    Add files via upload

---
+++ src/test/java/Sender/SendDataTest.java
@@ -0,0 +1,18 @@
+package TestDrivenRange;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import Sender.SendData;
+
+import org.junit.Test;
+
+public class SendDataTest{
+
+  @Test
+  public void testSendData() {
+   assertNotNull(new SendData())
+  }
+
+}
