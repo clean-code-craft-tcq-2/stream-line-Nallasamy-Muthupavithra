3c87a3d936dd804d5c610f632e32a1e5f72a7db6
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-28 23:58:31 +0530

    Update ReceiverTest.cpp

--- Receiver/ReceiverTest.cpp
+++ Receiver/ReceiverTest.cpp
@@ -8,7 +8,7 @@ TEST_CASE("Tests to check if data is correctly read from file")
   float Temperature[NO_OF_READINGS] = {0};
   BMSReceiver(Current, Temperature);
   
-  float expectedoutput[2][2] = {{00,00}, {00,00}};
+  float expectedoutput[2][2] = {{-9.9594,24.5782}, {-12.7969,7.8247}};
   for(int i=0;i<2;i++)
   {
     REQUIRE(Current[i] == expectedoutput[i][0]);
