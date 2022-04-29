c3b88f16debc56b11ab04ddb7cf00d3dc1638dbb
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-29 10:44:04 +0530

    Update Receiver.c

--- Receiver/Receiver.c
+++ Receiver/Receiver.c
@@ -5,7 +5,7 @@ void GetSensorDataFromConsole(float* Current, float* Temperature)
   FILE* fp = fopen("./BMS_DataParameter.txt","r");
   float Curr_readings, Temp_readings;
   
-  for(int i=0; fscanf(fp,"%f,%f \n", &Curr_readings,&Temp_readings)!=EOF; i++)
+  for(int i=0; fscanf(fp,"%f \t %f \n", &Curr_readings,&Temp_readings)!=EOF; i++)
     {
       *(Current+i) = Curr_readings;
       *(Temperature+i) = Temp_readings;
