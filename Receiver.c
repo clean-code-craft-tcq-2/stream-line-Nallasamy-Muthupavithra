836c62a3b27c185f638e25bf1129fc5d20a94736
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-30 00:05:50 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -5,7 +5,7 @@
 void GetSensorDataFromConsole()
 {
    char ReadString[1000] = {10,20,30,40};
-   for(int i=0;i<NO_OF_READINGS ; i++)
+   //for(int i=0;i<NO_OF_READINGS ; i++)
    //{
    char *tokencheck;
    //scanf("%s", ReadString);
