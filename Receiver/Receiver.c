9392966d156be7f38a3104ca1134e64bb83c2451
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-29 16:42:04 +0530

    Update Receiver.c

--- Receiver/Receiver.c
+++ Receiver/Receiver.c
@@ -3,7 +3,7 @@
 void GetSensorDataFromConsole(float* Current, float* Temperature)
 {
    char ReadString[400];
-   for(int i=0;i<2 ; i++)
+   for(int i=0;i<NO_OF_READINGS ; i++)
    {
    char *token;
    scanf("%s", ReadString);
