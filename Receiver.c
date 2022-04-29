b8cdaf7999604fc3b7a85eef3465f62adcec7861
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-29 19:23:53 +0530

    Add files via upload

---
+++ Receiver.c
@@ -0,0 +1,87 @@
+#include "Receiver.h"
+
+void GetSensorDataFromConsole(float Current, float Temperature)
+{
+   char ReadString[400];
+   for(int i=0;i<NO_OF_READINGS ; i++)
+   {
+   char *token;
+   scanf("%s", ReadString);
+   token = strtok (ReadString, ",");
+   //printf( "%s\n", token );
+   Current[i] = atof(token);
+   //printf ("Current[%d] %4f\n",i,Current[i]);
+   token = strtok (NULL, ",");
+   //printf( "%s\n", token );
+   Temperature[i]= atof(token);
+   //printf("Temperature[%d]: %.4f\n", i,Temperature[i]);
+   }
+}
+
+float GetMaxReadingValue(float *BMSParameter)
+{
+  int readingIndex;
+  float MaxReadingValue = BMSParameter[0];
+  
+  for(readingIndex = 0; readingIndex < NO_OF_READINGS; readingIndex++)
+  {
+    if(BMSParameter[readingIndex] > MaxReadingValue)
+    {
+      MaxReadingValue = BMSParameter[readingIndex];
+    }
+  }
+  
+  return MaxReadingValue;
+}
+
+float GetMinReadingValue(float *BMSParameter)
+{
+  int readingIndex;
+  float MinReadingValue = BMSParameter[0];
+  
+  for(readingIndex = 0; readingIndex < NO_OF_READINGS; readingIndex++)
+  {
+    if(BMSParameter[readingIndex] < MinReadingValue)
+    {
+      MinReadingValue = BMSParameter[readingIndex];
+    }
+  }
+  
+  return MinReadingValue;
+}
+
+float GetSMAValue(float *BMSParameter)
+{
+  float SMAValue = 0.0;
+  float Sum = 0.0;
+  int readingIndex = 0;
+  for(readingIndex = (NO_OF_READINGS-5); readingIndex < NO_OF_READINGS; readingIndex++)
+  {
+    Sum += BMSParameter[readingIndex];
+  }
+  
+  SMAValue = Sum/5;
+  
+  return SMAValue;
+  
+}
+
+void PrintReceivedDataOnConsole(float *BMSParameter, float MaxValue, float Minvalue, float SMA)
+{
+  int readingIndex = 0;
+  printf("Data received from sender\n");
+  
+  for(readingIndex = 0; readingIndex < NO_OF_READINGS; readingIndex++)
+  {
+    printf("%f\n",BMSParameter[readingIndex]);
+  }
+  printf("Max value: %f, Min value: %f, SMA: %f\n",MaxValue,Minvalue,SMA);
+  
+}
+
+void BMSReceiver(float Current, float Temperature)
+{
+  GetSensorDataFromConsole(Current, Temperature);
+  PrintReceivedDataOnConsole(Current,GetMaxReadingValue(Current),GetMinReadingValue(Current),GetSMAValue(Current));
+  PrintReceivedDataOnConsole(Temperature,GetMaxReadingValue(Temperature),GetMinReadingValue(Temperature),GetSMAValue(Temperature));  
+}
