5865843c0ff0282cb5137781cb2d35b0a1f1908a
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-30 15:33:59 +0530

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -4,9 +4,9 @@
 #include <string.h>
 
 #define NO_OF_READINGS 50
-void GetSensorDataFromConsole(float* Current, float* Temperature);
+void GetSensorDataFromConsole(float *Current, float *Temperature);
 float GetMaxReadingValue(float *BMSParameter);
 float GetMinReadingValue(float *BMSParameter);
 float GetSMAValue(float *BMSParameter);
 void PrintReceivedDataOnConsole(float *BMSParameter, float MaxValue, float Minvalue, float SMA);
-void BMSReceiver(float* Current, float* Temperature);
+void BMSReceiver(float *Current, float *Temperature);
