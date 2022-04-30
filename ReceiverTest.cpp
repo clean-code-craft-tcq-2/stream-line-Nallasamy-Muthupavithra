9a53918f9f96667085e708801030d98558168ab6
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-30 17:00:39 +0530

    Update ReceiverTest.cpp

--- ReceiverTest.cpp
+++ ReceiverTest.cpp
@@ -4,9 +4,13 @@
 
 TEST_CASE("Test to check Receiver statistics") 
 { 
-   BatteryChargingParameters ExpectedData[NO_OF_READINGS], Max, Min, SMA;
-   BMSReceiver(BatteryChargingParameters.Current, BatteryChargingParameters.Temperature);
    float Current_loc, Temperature_loc;
+   float Current[NO_OF_READINGS] = {0};
+   float Temperature[NO_OF_READINGS] = {0};
+   float ExpectedCurrent[NO_OF_READINGS] = {0};
+   float ExpectedTemperature[NO_OF_READINGS] = {0};
+   
+   BMSReceiver(Current, Temperature);
     
    FILE * file= fopen("./BMS_DataFromConsole.txt","r");                                                                  
    for(int i = 0 ; i < NO_OF_READINGS; i++)
@@ -15,27 +19,27 @@ TEST_CASE("Test to check Receiver statistics")
      {
         for(int i=0;fscanf(file, "%f,%f\n", &Current_loc,&Temperature_loc)!=EOF ;i++)
         {
-            ExpectedData[i].Current = Current_loc;
-            ExpectedData[i].Temperature = Temperature_loc;
+            ExpectedCurrent[i] = Current_loc;
+            ExpectedTemperature[i] = Temperature_loc;
         }
      }
        
-     REQUIRE( (BatteryChargingParameters.Current[i] - ExpectedData[i].Current) <= 0.001);
-     REQUIRE( (BatteryChargingParameters.Current[i] - ExpectedData[i].Temperature) <= 0.001);
+     REQUIRE( (Current[i] - ExpectedCurrent[i]) <= 0.001);
+     REQUIRE( (Current[i] - ExpectedTemperature[i]) <= 0.001);
    }
    fclose(file);
  
-    Min.Current=  GetMaxReadingValue(Current);
-    Max.Current = GetMinReadingValue(Current);
-    SMA.Current = GetSMAValue(Current);
-    Min.Temperature=  GetMaxReadingValue(Temperature);
-    Max.Temperature = GetMinReadingValue(Temperature);
-    SMA.Temperature = GetSMAValue(Temperature);
+    float MinCurrrent =  GetMaxReadingValue(Current);
+    float MaxCurrent = GetMinReadingValue(Current);
+    float SMACurrent = GetSMAValue(Current);
+    float MinTemperature=  GetMaxReadingValue(Temperature);
+    float MaxTemperature = GetMinReadingValue(Temperature);
+    float SMATemperature = GetSMAValue(Temperature);
    
-    REQUIRE( abs(Min.Current - (-13.774300) ) <=0.001);
-    REQUIRE( abs(Max.Current - 14.962000 ) <=0.001);
-    REQUIRE( abs(SMA.Current - 1.511800 ) <=0.001);
-    REQUIRE( abs(Min.Temperature - 0.216200 ) <=0.001);
-    REQUIRE( abs(Max.Temperature - 43.833599 ) <=0.001);
-    REQUIRE( abs(SMA.Temperature - 23.042278 ) <=0.001);
+    REQUIRE( abs(MinCurrent - (-13.774300) ) <=0.001);
+    REQUIRE( abs(MaxCurrent - 14.962000 ) <=0.001);
+    REQUIRE( abs(SMACurrent - 1.511800 ) <=0.001);
+    REQUIRE( abs(MinTemperature - 0.216200 ) <=0.001);
+    REQUIRE( abs(MaxTemperature - 43.833599 ) <=0.001);
+    REQUIRE( abs(SMATemperature - 23.042278 ) <=0.001);
 }
