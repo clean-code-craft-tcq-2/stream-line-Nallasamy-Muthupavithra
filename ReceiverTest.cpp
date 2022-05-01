c3a5342caa553a7e31172f25dccd931e05d1680c
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-05-01 16:28:27 +0530

    Update ReceiverTest.cpp

--- ReceiverTest.cpp
+++ ReceiverTest.cpp
@@ -5,10 +5,8 @@
 TEST_CASE("Test to check Receiver statistics") 
 { 
    float Current_loc, Temperature_loc;
-   float Current[NO_OF_READINGS] = {0};
-   float Temperature[NO_OF_READINGS] = {0};
-   float ExpectedCurrent[NO_OF_READINGS] = {0};
-   float ExpectedTemperature[NO_OF_READINGS] = {0};
+   float Current[NO_OF_READINGS], Temperature[NO_OF_READINGS] = {0};
+   float ExpectedCurrent[NO_OF_READINGS], ExpectedTemperature[NO_OF_READINGS] = {0};
    
    BMSReceiver(Current, Temperature);
     
@@ -29,17 +27,17 @@ TEST_CASE("Test to check Receiver statistics")
    }
    fclose(file);
  
-    float MinCurrent =  GetMinReadingValue(Current);
-    float MaxCurrent = GetMaxReadingValue(Current);
-    float SMACurrent = GetSMAValue(Current);
-    float MinTemperature=  GetMinReadingValue(Temperature);
-    float MaxTemperature = GetMaxReadingValue(Temperature);
-    float SMATemperature = GetSMAValue(Temperature);
+   float MinCurrent =  GetMinReadingValue(Current);
+   float MaxCurrent = GetMaxReadingValue(Current);
+   float SMACurrent = GetSMAValue(Current);
+   float MinTemperature=  GetMinReadingValue(Temperature);
+   float MaxTemperature = GetMaxReadingValue(Temperature);
+   float SMATemperature = GetSMAValue(Temperature);
    
-    REQUIRE( abs(MinCurrent - (-13.774300) ) <=0.001);
-    REQUIRE( abs(MaxCurrent - 14.962000 ) <=0.001);
-    REQUIRE( abs(SMACurrent - 1.511800 ) <=0.001);
-    REQUIRE( abs(MinTemperature - 0.216200 ) <=0.001);
-    REQUIRE( abs(MaxTemperature - 43.833599 ) <=0.001);
-    REQUIRE( abs(SMATemperature - 23.042278 ) <=0.001);
+   REQUIRE( abs(MinCurrent - (-13.774300) ) <=0.001);
+   REQUIRE( abs(MaxCurrent - 14.962000 ) <=0.001);
+   REQUIRE( abs(SMACurrent - 1.511800 ) <=0.001);
+   REQUIRE( abs(MinTemperature - 0.216200 ) <=0.001);
+   REQUIRE( abs(MaxTemperature - 43.833599 ) <=0.001);
+   REQUIRE( abs(SMATemperature - 23.042278 ) <=0.001);
 }
